5baf51897b2d370d09c5aa943f8d0921a94bbc94
James DeVore <jdevore4592@gmail.com>
2018-12-06 14:45:42 -0500

    updating search return function

--- client/src/App.js
+++ client/src/App.js
@@ -16,28 +16,7 @@ class App extends Component {
   // //      .then(users => this.setState({ users }));
   //   }
 
-  fetchUserSearch(searchObject) {
-    let baseUrl = "https://api.case.law/v1/cases/";
-    let name = `?name_abbreviation=${searchObject.name}`;
-    let start = `&decision_start_date=${searchObject.startDate}`;
-    let end = `&decision_end_date=${searchObject.endDate}`;
-    let category = `&search=${searchObject.category}`;
-    if (searchObject.startDate || searchObject.endDate) {
-      if (!/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(searchObject.startDate) || !/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(searchObject.endDate)) {
-        alert("Please enter both dates as YYYY-MM-DD")
-        return
-      }
-    };
-      return {
-        name_abbreviation:searchObject.name,
-        decision_start_api:searchObject.startDate,
-        decision_end_date:searchObject.endDate,
-        search:searchObject.category
-      }
-
-      }
-
-  }
+  
 
   render() {
     return (
