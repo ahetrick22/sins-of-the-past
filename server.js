2d30649c10983fe533434cbbb56ea53cd9e28135
Alicia Hetrick <ahetrick22@gmail.com>
2018-12-06 15:47:33 -0500

    posting works

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const querystring = require('querystring');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
@@ -13,6 +15,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/api/hello', (req, res) => {
+
   request(
   "https://api.case.law/v1/cases/?search=piracy&decision_date_min=1800-01-01&decision_date_max=1850-01-01",
   { json: true },
@@ -26,10 +29,20 @@ app.get('/api/hello', (req, res) => {
 
 app.post('/api/world', (req, res) => {
   const data = JSON.parse(JSON.stringify(req.body));
-  console.log('parsed data', data);
-  res.send(
-    `Received POST request. This is what you sent me: ${JSON.stringify(data)}`,
-  );
+  console.log('data',data);
+  const urlQueryString = querystring.stringify(req.body);
+  console.log('urlquery',urlQueryString)
+  request('https://api.case.law/v1/cases/?' + urlQueryString,
+  { json: true },
+  (err, resp, body) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(body);
+     res.send(body);
+  })
+
 });
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
