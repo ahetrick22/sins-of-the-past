c96643579cac36c3634a18e232c262ac4713e725
Alicia Hetrick <ahetrick22@gmail.com>
2018-12-06 11:57:32 -0500

    adjusted folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,45 @@
 import React, { Component } from 'react';
+import SearchBar from './components/search-bar'
 import './App.css';
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
-render() {
+class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => console.log(res.json()))
+//      .then(users => this.setState({ users }));
+  }
+
+  fetchUserSearch(searchObject) {
+    let baseUrl = "https://api.case.law/v1/cases/";
+    let name = `?name_abbreviation=${searchObject.name}`;
+    let start = `&decision_start_date=${searchObject.startDate}`;
+    let end = `&decision_end_date=${searchObject.endDate}`;
+    let category = `&search=${searchObject.category}`;
+    if (searchObject.startDate || searchObject.endDate){
+    if (!/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(searchObject.startDate) || !/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(searchObject.endDate)){
+      alert("Please enter both dates as YYYY-MM-DD")
+      return
+    }
+    };
+
+    let searchUrl = baseUrl + name + start + end + category;
+    console.log(searchUrl)
+     
+  }
+
+  render() {
     return (
-        <div>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+      <div className="App">
+      <SearchBar searchFunction={this.fetchUserSearch} />
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
 }
+
 export default App;
