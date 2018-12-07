0d5bf1cb2c1d8d162d39da1edc8afaf7aef7f882
Alicia Hetrick <ahetrick22@gmail.com>
2018-12-07 09:24:13 -0500

    fixed formatting of case items and limited display to 10

--- client/src/components/case-item.js
+++ client/src/components/case-item.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 
 const CaseItem = (props) => {
+  console.log(props.index);
 
-
-  console.log(props)
-
-
-  return (
-
-    <div className="container-c left slide-left card">
-      <div className="content">
-        <h2>{props.case.decision_date.slice(0,4)}</h2>
-        <p><strong>{props.case.name}</strong></p>
-        <p> I am a case summary </p>
+  if (props.index % 2 == 0) {
+    return (
+      <div className="container-c left slide-right">
+        <div className="content">
+          <h2>{props.case.decision_date.slice(0,4)}</h2>
+          <p><strong>{props.case.name}</strong></p>
+          <p> I am a case summary </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="container-c right slide-left">
+        <div className="content">
+          <h2>{props.case.decision_date.slice(0,4)}</h2>
+          <p><strong>{props.case.name}</strong></p>
+          <p> I am a case summary </p>
+        </div>
+      </div>
+    )
+  }
+
+
 }
 
 export default CaseItem
